97b6d269cca5185446631c71cbf9ee2dc5288c52
Eko Prastyo <604912+temon@users.noreply.github.com>
2024-05-28 19:43:00 +1000

    hello world endpoint

--- src/main/java/com/sewonlab/mn/hello/HelloWorldController.java
+++ src/main/java/com/sewonlab/mn/hello/HelloWorldController.java
@@ -1,5 +1,14 @@
 package com.sewonlab.mn.hello;
 
 
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+
+@Controller("/hello")
 public class HelloWorldController {
+
+    @Get(value = "/", produces = "text/plain")
+    public String HelloWorld() {
+        return "Hello World!";
+    }
 }
