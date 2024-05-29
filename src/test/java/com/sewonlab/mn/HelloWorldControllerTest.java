9e2acaf381cccaf053bc84c48b16814c52a36f31
Eko Prastyo <604912+temon@users.noreply.github.com>
2024-05-29 19:38:07 +1000

    hello world endpoint - unit test

---
+++ src/test/java/com/sewonlab/mn/HelloWorldControllerTest.java
@@ -0,0 +1,32 @@
+package com.sewonlab.mn;
+
+import io.micronaut.http.HttpStatus;
+import io.micronaut.http.client.HttpClient;
+import io.micronaut.http.client.annotation.Client;
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import jakarta.inject.Inject;
+
+@MicronautTest
+class HelloWorldControllerTest {
+
+    @Inject
+    @Client("/")
+    HttpClient httpClient;
+
+    @Test
+    void testHelloWorld() {
+        String response = httpClient.toBlocking().retrieve("/hello");
+        Assertions.assertEquals("Hello World!", response);
+    }
+
+    @Test
+    void testHelloWorldReturnSuccess200() {
+        var response = httpClient.toBlocking().exchange("/hello", String.class);
+        Assertions.assertEquals(HttpStatus.OK, response.getStatus());
+        Assertions.assertEquals("Hello World!", response.body());
+    }
+}
