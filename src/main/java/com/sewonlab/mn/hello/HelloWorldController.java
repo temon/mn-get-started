c2b7424efc047016b1243f097c31f3f59c4055b2
Eko Prastyo <604912+temon@users.noreply.github.com>
2024-06-04 17:49:32 +1000

    constructor - dependency injection

--- src/main/java/com/sewonlab/mn/hello/HelloWorldController.java
+++ src/main/java/com/sewonlab/mn/hello/HelloWorldController.java
@@ -3,15 +3,17 @@ package com.sewonlab.mn.hello;
 
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
-import jakarta.inject.Inject;
 import jakarta.inject.Singleton;
 
 @Singleton
 @Controller("/hello")
 public class HelloWorldController {
 
-    @Inject
-    private HelloWorldService helloWorldService;
+    private final HelloWorldService helloWorldService;
+
+    public HelloWorldController(final HelloWorldService helloWorldService) {
+        this.helloWorldService = helloWorldService;
+    }
 
     @Get(value = "/", produces = "text/plain")
     public String HelloWorld() {
