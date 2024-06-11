162f6195c80a41486e3e70d79099c3973ce84877
Eko Prastyo <604912+temon@users.noreply.github.com>
2024-06-11 18:13:12 +1000

    accessing service from application context

--- src/main/java/com/sewonlab/mn/Application.java
+++ src/main/java/com/sewonlab/mn/Application.java
@@ -1,10 +1,20 @@
 package com.sewonlab.mn;
 
+import com.sewonlab.mn.hello.HelloMicronautService;
+import com.sewonlab.mn.hello.HelloWorldController;
+import com.sewonlab.mn.hello.HelloWorldService;
 import io.micronaut.runtime.Micronaut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Application {
 
+    private static final Logger LOG = LoggerFactory.getLogger(Application.class);
+
     public static void main(String[] args) {
-        Micronaut.run(Application.class, args);
+        var context = Micronaut.run(Application.class, args);
+
+        LOG.info("Message from service: {}", context.getBean(HelloWorldService.class).sayHello());
+        LOG.info("Message from service: {}", context.getBean(HelloMicronautService.class).sayHello());
     }
 }
